62f790fcf8ab77228d5caef7b8e9284e96036426
Imperial Construct - Jason <imperialconstructco@gmail.com>
2019-05-05 14:32:50 +0200

    implemented validation and data binding for route form

--- src/app/create-destination/create-destination.component.ts
+++ src/app/create-destination/create-destination.component.ts
@@ -13,12 +13,41 @@ export class CreateDestinationComponent implements OnInit {
 	@Input() route: Route = new Route()
 	dateConv = new DateConverter()
 
+	added = false
+	error = {
+		country: "",
+		city: "",
+		depDate: ""
+	}
+
 	ngbDestDep =  this.route.departureDate === null ? 
 		null : this.dateConv.dateToNgbDate(this.route.departureDate)
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addRoute(route: Route) {
+  	let validated = true
+  	this.added = true
+  	this.error = { country: '', city: '', depDate: '' }
+  	if (route.country === '') {
+  		this.error.country = "Negara tujuan tidak boleh kosong!"
+  		validated = false
+  	}
+  	if (route.city === '') {
+  		this.error.city = "Kota tujuan tidak boleh kosong!"
+  		validated = false
+  	}
+  	if (route.departureDate === null) {
+  		this.error.depDate = "Tanggal keberangkatan tidak boleh kosong!"
+  		validated = false
+  	}
+
+  	if (validated) {
+  		//Send the route object
+  	}
   }
 
   onDateSelected(which, event) {
