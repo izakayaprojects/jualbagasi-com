114484154b3a3e158a126b7365924343f849e6dd
Imperial Construct - Jason <imperialconstructco@gmail.com>
2019-05-05 13:48:14 +0200

    implemented datepicker in route window like po

--- src/app/create-destination/create-destination.component.ts
+++ src/app/create-destination/create-destination.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { DateConverter } from "../_models/utils"
 import { Route } from "../_models/order";
 
 @Component({
   selector: 'app-create-destination',
   templateUrl: './create-destination.component.html',
-  styleUrls: ['./create-destination.component.css']
+  styleUrls: ['./create-destination.component.css', "../app.component.css"]
 })
 export class CreateDestinationComponent implements OnInit {
 
-	@Input() route: Route
+	@Input() route: Route = new Route()
+	dateConv = new DateConverter()
+
+	ngbDestDep =  this.route.departureDate === null ? 
+		null : this.dateConv.dateToNgbDate(this.route.departureDate)
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onDateSelected(which, event) {
+  	this.route[which] = this.dateConv.ngbDateToDate(event)
+  	if (which === "departureDate") {
+  		this.ngbDestDep = event
+  	}
   }
 
 }
