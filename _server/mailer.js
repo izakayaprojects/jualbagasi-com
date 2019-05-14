6c80f15df1ba5860b3ca2899a7508cc0f209cbdd
Imperial Construct - Jason <imperialconstructco@gmail.com>
2019-05-14 19:48:29 +0200

    added nodemailer

---
+++ _server/mailer.js
@@ -0,0 +1,12 @@
+const mailer = require("nodemailer")
+
+module.exports = {
+	send_email_confirmation: function(recipient, confirmationToken) {
+		// TODO construct email
+		
+	},
+
+	send_mail: function(from, to, title, body) {
+		// TODO construct general email
+	}
+}
